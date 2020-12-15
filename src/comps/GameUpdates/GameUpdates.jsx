29e231b4516d2af9587d0a074a8d64398251db82
ZIHAN <zihanforall@gmail.com>
2020-12-15 20:15:13 +0600

    Removed the line

--- src/comps/GameUpdates/GameUpdates.jsx
+++ src/comps/GameUpdates/GameUpdates.jsx
@@ -12,7 +12,7 @@ return(
  <GameUpdatesCardImageLeft heading="What Machine Gun do you guys want on this beast?" image="/images/Humba.png"/>
  <GameUpdatesCardImageRight heading="M4A1+Aimpoint CompM4s" image="/images/Aimpoint.png"/>
  
- <GameUpdatesCardImageLeft heading="'I want my son to be an engineer. But he, he wants to join the army. What kind of dream is this?'" image="/images/img1.png"/>
+ <GameUpdatesCardImageLeft heading="'I want my son to be an engineer. But he, he wants to join the army.'" image="/images/img1.png"/>
   </>
 )
 }
