1500b92eb673dab813a8bc94df6f776b85adc402
ZIHAN <zihanforall@gmail.com>
2020-12-23 20:15:03 +0600

    Added the API to post files.

--- src/comps/GameUpdates/GameUpdates.jsx
+++ src/comps/GameUpdates/GameUpdates.jsx
@@ -2,17 +2,49 @@ import React from 'react';
 import GameUpdatesCardImageLeft from './GameUpdatesCardImageLeft';
 import GameUpdatesCardImageRight from './GameUpdatesCardImageRight';
 import "./GameUpdates.css";
+import { useEffect,useState } from 'react';
 
 const GameUpdates = ()=>{
+const [postData,setPostData] = useState([]);
+useEffect(()=>{
+
+  fetch('/allposts',{
+    method:"GET",
+    headers:{
+      "Content-Type":"application/json"
+    }
+  }).then(res=>res.json())
+  .then(result=>{
+    setPostData(result.posts)
+    console.log(result.posts)
+  })
+  .catch(err=>{console.log(err)})
+   
+},[])
+
 return(
-  <>
-  
- <GameUpdatesCardImageRight heading="Meet Captain Akbar. More details coming soon." 
- image="/images/CaptainAkbar.png"/>
- <GameUpdatesCardImageLeft heading="What Machine Gun do you guys want on this beast?" image="/images/Humba.png"/>
- <GameUpdatesCardImageRight heading="M4A1+Aimpoint CompM4s" image="/images/Aimpoint.png"/>
+ <> 
+{postData.length?
+<div>
+  {postData[0]?
+ <GameUpdatesCardImageRight heading={postData[0].description}
+ image={postData[0].pic}/>:"No items to show"
+  }
+  {postData[1]?
+    <GameUpdatesCardImageLeft heading={postData[1].description} image={postData[1].pic}/>:"No items to show"
+  }
+  {postData[2]?
+    <GameUpdatesCardImageRight heading={postData[2].description} image={postData[2].pic}/>:"No items to show"
+  }
  
- <GameUpdatesCardImageLeft heading="'I want my son to be an engineer. But he, he wants to join the army.'" image="/images/img1.png"/>
+  {postData[3]?
+    <GameUpdatesCardImageLeft heading={postData[3].description} image={postData[3].pic}/>:"No items to show"
+  }
+
+ </div>
+ 
+ :"No pics to show"
+}
   </>
 )
 }
